0045c8606ee1f3638bd0125d0d48a72664889c0d
Anton Makarov <makarov_13@mail.ru>
2026-07-03 10:17:27 +0300

    Unify theme color handling

--- src/main.tsx
+++ src/main.tsx
@@ -11,6 +11,7 @@ import './styles/overview-schedule.css'
 import './styles/app-ui.css'
 import './styles/components.css'
 import './styles/mobile-schedule.css'
+import './styles/theme-overrides.css'
 import './styles/print.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<React.StrictMode><ErrorBoundary><App /></ErrorBoundary></React.StrictMode>)
