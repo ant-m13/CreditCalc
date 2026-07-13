96ae8034ef01f1733f9049ef641cf390b8d0a1f5
Anton Makarov <makarov_13@mail.ru>
2026-07-13 12:32:00 +0300

    chore(release): document version 1.7.3

--- src/releaseVersion.test.ts
+++ src/releaseVersion.test.ts
@@ -4,9 +4,9 @@ import { APP_VERSION, CHANGELOG } from './version'
 
 describe('release version', () => {
   it('показывает ту же версию, что package и верхняя секция changelog', () => {
-    expect(packageJson.version).toBe('1.7.2')
+    expect(packageJson.version).toBe('1.7.3')
     expect(APP_VERSION).toBe(packageJson.version)
     expect(CHANGELOG[0]).toMatchObject({ version: packageJson.version, date: '13.07.2026' })
-    expect(CHANGELOG[0].items).toContain('Версия приложения повышена до 1.7.2.')
+    expect(CHANGELOG[0].items).toContain('Версия приложения повышена до 1.7.3.')
   })
 })
